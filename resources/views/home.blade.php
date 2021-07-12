4e84d04b66abef6ae9b7e91d45f310420dd9e138
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-07-12 17:47:21 +0100

    - Display all movies by foreach loop in views/home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,91 +10,29 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
+
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+       <!-- Website header -->
+       <header>
+            <h1 class="heading">Laravel Model Controller</h1>
+        </header>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+        <!-- Website content -->
+        <main>
+            <div class="container">
+                @foreach($movies as $movie)
+                    <div class="movie-card">
+                        <h2 class="title">Title: {{ $movie->title }}</h2>
+                        <h3 class="original-title">Original title: {{ $movie->original_title }}</h3>
+                        <div class="nationality">Nationality: {{ ucfirst($movie->nationality) }}</div>
+                        <div class="date">Released on: {{ $movie->date }}</div>
+                        <div class="vote">Vote: {{ $movie->vote }}</div>
+                    </div>
+                @endforeach
             </div>
-        </div>
+        </main>
+            
+        
     </body>
 </html>
